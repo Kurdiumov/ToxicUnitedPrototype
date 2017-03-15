d7875fff75b812fd9897abee849dcc0255ac2019
Kurdiumov <rostyslav.kurdiumov@gmail.com>
2017-03-15 12:49:28 +0100

    Code cleanup

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-
-
     enum MovingModes
     {
         Player,
@@ -15,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public Transform LookTarget;
 
     MovingModes moveMode;
+
     //Interaction
     public float interactDistance = 3.0f;
 
@@ -23,99 +22,47 @@ public class PlayerController : MonoBehaviour
     public float sprintSpeed = 12.0f;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
-    private Vector3 moveVector;
-    ///CharacterController controller;
-    //public CharacterController characterController { get { return controller; } }
-    float ladderCd;
-
-
+    
     //   Camera:
     public float mouseSensitivity = 100.0f;
     private float rotationX;
     private float rotationY;
-    public Camera cam;
+    public Camera camera;
+ 
     //public Camera minimapCam;
     public float viewRange = 20.0f;
-    public Vector3 roomView = new Vector3(0.0f , 90.0f, -6.0f);
-    Vector3 playerView = new Vector3(0.0f , 0.5f , 0.0f);
+    public Vector3 roomView = new Vector3(0.0f, 90.0f, -6.0f);
+    Vector3 playerView = new Vector3(0.0f, 0.5f, 0.0f);
 
-    RaycastHit ceiling;
-    RaycastHit hitItem;
-
-
+    public RaycastHit Ceiling { get; private set; }
+    public RaycastHit HitItem { get; private set; }
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         moveMode = MovingModes.Player;
-        //controller = GetComponent<CharacterController>();
         Cursor.visible = false;
-        moveVector = Vector3.zero;
     }
     void Update()
     {
-        //showhideCursor();
-        if(moveMode == MovingModes.Player)
+        if (moveMode == MovingModes.Player)
         {
-            //Move();
-            cameraControl();
+            CameraControl();
         }
 
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             SwitchControl();
         }
-
-        
     }
-
-    void OnTriggerEnter(Collider other)
-    {
-    }
-
-
-    void OnTriggerExit(Collider other)
-    {
-    }
-    /*
-    void Move()
-    {
-        if (controller.isGrounded && moveMode == MovingModes.Player)
-        {
-            moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveVector = transform.TransformDirection(moveVector);
-
-            moveVector *= speed;
-            /*if (Input.GetButton("Fire3"))
-            {
-                moveVector *= sprintSpeed;
-            }
-            else
-            {
-                moveVector *= speed;
-            }*/
-
-            /*
-            if (Input.GetButton("Jump"))
-            {
-                moveVector.y = jumpSpeed;
-            }
-
-
-
-        }
-        controller.Move(moveVector * Time.deltaTime);
-    }*/
 
     void RoomMove()
     {
 
     }
 
-    void cameraControl()
+    void CameraControl()
     {
-
-        
         float mouseX;
         float mouseY;
 
@@ -134,139 +81,62 @@ public class PlayerController : MonoBehaviour
             if (rotationY > 80)
             {
                 rotationY = 80;
-
             }
-            else
+            else if (rotationY < -80)
             {
-                if (rotationY < -80)
-                {
-                    rotationY = -80;
-
-                }
+                rotationY = -80;
             }
 
             Quaternion rotatePlayer = Quaternion.Euler(0.0f, rotationX, 0.0f);
             Quaternion rotateCam = Quaternion.Euler(rotationY, rotationX, 0.0f);
-           
-            transform.rotation = rotatePlayer;
-            cam.transform.rotation = rotateCam;
-            //minimapCam.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
-
-
-            //Debug.Log(rotationY);
-
-
-
-            Vector3 rotray = new Vector3(0.0f, 0.0f, 1.0f);
-            // Vector3 raypose = new Vector3(0.0f, 0.0f, 0.0f);
-
-
-
-
-
-            
-        }
-
-        /// Third person
-        if (moveMode ==  MovingModes.Strategic)
-        {
-            if (rotationY > 80)
-            {
-                rotationY = 80;
-
-            }
-            else
-            {
-                if (rotationY < 10)
-                {
-                    rotationY =  10;
-
-                }
-            }
-
-            Quaternion rotatePlayer = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-            //Quaternion rotateCam = Quaternion.Euler( rotationY, rotationX, 0.0f);
-            Quaternion rotateCam = Quaternion.Euler(60.0f, 0.0f, 0.0f);
 
             transform.rotation = rotatePlayer;
-            cam.transform.rotation = rotateCam;
-
-            Vector3 rotray = new Vector3(0.0f, 0.0f, 1.0f);
-            
-
-            //Interaktcja w widoku z gory
-            /*
-            Debug.DrawRay(transform.position, rotatePlayer * rotray * interactDistance, Color.magenta);
-            if (Physics.Raycast(transform.position, rotatePlayer * rotray, out hitItem, interactDistance))
-            {
-                if (hitItem.collider.tag == "Interactive")
-                {
-                    UIEvents.TurnInteraction(true);
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        //Debug.Log("Cos niby kliklem");
-                        Interact();
-                    }
-                }
-
-            }
-            else
-            {
-                UIEvents.TurnInteraction(false);
-            }*/
+            camera.transform.rotation = rotateCam;
         }
-        
     }
 
     void SwitchControl()
     {
-        //Debug.Log("Tryb z gory");
-        if(moveMode == MovingModes.Player)
+        if (moveMode == MovingModes.Player)
         {
             moveMode = MovingModes.Strategic;
-            this.playerView = cam.transform.localPosition;
-            //Debug.Log(playerView);
-            cam.transform.localPosition = this.roomView;
-            cam.transform.LookAt(LookTarget, Vector3.up);
+            this.playerView = camera.transform.localPosition;
+            camera.transform.localPosition = this.roomView;
+            camera.transform.LookAt(LookTarget, Vector3.up);
 
-            moveVector = Vector3.zero;
-
+            
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
         else
         {
             moveMode = MovingModes.Player;
-            cam.transform.localPosition = this.playerView;
+            camera.transform.localPosition = this.playerView;
 
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
     }
 
-    void showhideCursor()
+    void ToggleCursor()
     {
         if (Input.GetButtonDown("Fire2") && Cursor.visible == false)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
-        else
+        else if (Input.GetButtonDown("Fire2") && Cursor.visible == true)
         {
-            if (Input.GetButtonDown("Fire2") && Cursor.visible == true)
-            {
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
-            }
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
-    
 
-    private IEnumerator turnOffCam()
+    private IEnumerator TurnOffCam()
     {
-        cam.enabled = false;
+        camera.enabled = false;
         yield return new WaitForSeconds(1.0f);
-        cam.enabled = true;
+        camera.enabled = true;
         yield return 0;
     }
 }
