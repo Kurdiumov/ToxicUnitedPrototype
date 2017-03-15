86c37f7e1ef535579059c8531192044deafdb8d5
Adrian Zjawiński <Adrian Zjawiski>
2017-03-15 15:35:05 +0100

    Added unit and fixed GUI unit panel view

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject UnitArrayPool;
+
+    private List<Unit> AvailableUnits;
+
+	// Use this for initialization
+	void Start () {
+        AvailableUnits = new List<Unit>();
+        
+    }
+
+    public void addUnit(Unit unit) {
+        //UnitArrayPool.
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+    public int Type;
+
+    public string Name;
+    public Sprite Image;
+    public int Health;
+    public int Attack;
+    public int AttackRange;
+    public int AttackSpeed;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+}
