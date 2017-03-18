e9d08f97fe4dc8f1ee6318c3c1fb72231acdebe7
Kurdiumov <rostyslav.kurdiumov@gmail.com>
2017-03-18 19:33:52 +0100

    Knockback added when shooting. Shooting available one time in 3 seconds

--- Assets/Scripts/SniperControl.cs
+++ Assets/Scripts/SniperControl.cs
@@ -9,22 +9,30 @@ public class SniperControl : MonoBehaviour
     public Camera SniperCamera;
     public int Ammo;
     public int Attack;
+    public int TimeBetweenShoots;
+    public float KnockbackAmount;
     public AudioSource SniperShootSound;
     public AudioSource SniperOutOfAmmoSound;
-
-
+    
+    private float _knockbackOffset;
     private PlayerController _playerController;
     private Canvas _sniperSightCanvas;
-    private Text  ammoText;
+    private Text ammoText;
     private Image _sniperImage;
     private readonly float _mouseSensitivity = 30;
+    private float _timeFromLastShoot;
+
+    
+    private Vector2 startingPosition;
+
+    private float randx = (Random.Range(-30, 30)) / 10f;
+    private float randy = (Random.Range(-30, 30)) / 10f;
 
     void Start()
     {
         _playerController = gameObject.GetComponent<PlayerController>();
-
         _sniperSightCanvas = transform.Find("SniperSightCanvas").gameObject.GetComponent<Canvas>();
-        
+
         SniperCamera.enabled = false;
         ammoText = _sniperSightCanvas.GetComponentInChildren<Text>();
         ammoText.text = "Ammo: " + Ammo;
@@ -34,16 +42,19 @@ public class SniperControl : MonoBehaviour
 
     void Update()
     {
+        _timeFromLastShoot -= Time.deltaTime;
+
         if (PlayerController.moveMode == PlayerController.Mode.Sniper)
         {
             if (Input.GetMouseButton(1))
             {
-                _playerController.PlayerMove();
-                CameraControl();
                 if (Input.GetButtonDown("Fire1"))
                 {
-                    Shoot();
+                    if (_timeFromLastShoot <= 0)
+                        Shoot();
                 }
+                _playerController.PlayerMove();
+                CameraControl();
             }
             else
             {
@@ -57,10 +68,13 @@ public class SniperControl : MonoBehaviour
     {
         if (this.Ammo > 0)
         {
+            _timeFromLastShoot = TimeBetweenShoots;
             SniperShootSound.PlayOneShot(SniperShootSound.clip);
             Debug.Log("Shoot");
             this.Ammo--;
             ammoText.text = "Ammo: " + Ammo;
+            _knockbackOffset = KnockbackAmount;
+            startingPosition = new Vector2(_playerController.RotationX, _playerController.RotationY);
 
             RaycastHit hitInfo = new RaycastHit();
             if (Physics.Raycast(SniperCamera.ScreenPointToRay(Input.mousePosition), out hitInfo))
@@ -106,15 +120,27 @@ public class SniperControl : MonoBehaviour
 
     public void CameraControl()
     {
-
+        
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = -Input.GetAxis("Mouse Y");
 
-        if (PlayerController.moveMode == PlayerController.Mode.FirstPerson || PlayerController.moveMode == PlayerController.Mode.Sniper)
+        if (PlayerController.moveMode == PlayerController.Mode.FirstPerson ||
+            PlayerController.moveMode == PlayerController.Mode.Sniper)
         {
             _playerController.RotationX += mouseX * _mouseSensitivity * Time.deltaTime;
             _playerController.RotationY += mouseY * _mouseSensitivity * Time.deltaTime;
 
+
+            //knockback when shooting
+            if (_timeFromLastShoot == TimeBetweenShoots)
+            {
+                _playerController.RotationY -= _knockbackOffset;
+            }
+            _playerController.RotationY = Mathf.Lerp(_playerController.RotationY, _playerController.RotationY + _knockbackOffset, 0.1f);
+            _knockbackOffset = Mathf.Lerp(_knockbackOffset, 0f, 0.1f);
+
+
+            
             if (_playerController.RotationY > 80)
             {
                 _playerController.RotationY = 80;
